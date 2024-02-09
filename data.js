23fb9cf9370c5508a30843e2d83e2fdfe89f1ef7
Jeff Rogers <jrojers@gmail.com>
2024-02-09 14:19:51 -0600

    updating for noise

--- data.js
+++ data.js
@@ -1,9 +1,18 @@
 const data = {
-  "title": "hello",
-  "inputs": [
-    "6",
-    "5",
-    "2g",
-    ""
-  ]
+  title: "user story dev",
+  inputs: ["acceptance criteria", "coding standards"],
+  outcomes: [
+    "deployable artifact",
+    "passing tests",
+    "updated validation plan",
+    "satisfied acceptance criteria",
+    "satisfied customer",
+  ],
+  noise: [
+    "changing requirement",
+    "availability of team members",
+    "external dependencies",
+    "satisfied acceptance criteria",
+    "technical debt",
+  ],
 };
--- diagram.js
+++ diagram.js
@@ -6,13 +6,25 @@ const questions = [
   {
     type: "input",
     name: "title",
-    message: "What is the title of the slideshow?",
+    message: "What is the title of your function or system?",
   },
   {
     type: "input",
     name: "inputs",
     message:
       "What are the inputs to your system? (Enter a comma-separated list)",
+  },
+  {
+    type: "input",
+    name: "outcomes",
+    message:
+      "What are the outcomes of your system? (Enter a comma-separated list)",
+  },
+  {
+    type: "input",
+    name: "noise",
+    message:
+      "What are the noise factors of your system? (Enter a comma-separated list)",
   },
 ];
 
@@ -21,7 +33,12 @@ inquirer.prompt(questions).then(async (answers) => {
     let inputs = answers.inputs.split(",").map((input) => input.trim());
     answers.inputs = inputs;
 
-    // Write the answers to a JavaScript file
+    let outcomes = answers.outcomes.split(",").map((input) => input.trim());
+    answers.outcomes = outcomes;
+
+    let noise = answers.noise.split(",").map((input) => input.trim());
+    answers.noise = noise;
+
     const content = `const data = ${JSON.stringify(answers, null, 2)};`;
     await fs.writeFile("./data.js", content, "utf8");
 
@@ -33,7 +50,6 @@ inquirer.prompt(questions).then(async (answers) => {
             <meta charset="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-            <title>{{title}} P Diagram</title>
             <meta name="description" content="Generated Atriumn P-Diagram." />
 
             <link
@@ -97,96 +113,150 @@ inquirer.prompt(questions).then(async (answers) => {
         </head>
         <body>
             <header role="banner">
-            <nav role="navigation">
-                <p class="logo">
-                <a href="{{title}}-p-diagram.html" title="Atriumn P-Diagram"
-                    >P Diagram</a
-                >
-                </p>
-                <ul>
-                <li class="github">
-                    <a
-                    rel="external"
-                    href="https://github.com/jeff-atriumn/atriumn-p-diagrams"
-                    title="Github"
+                <nav role="navigation">
+                    <p class="logo">
+                    <a href="{{title}}-p-diagram.html" title="Atriumn P-Diagram"
+                        >P Diagram</a
                     >
-                    <svg class="fa-github">
-                        <use xlink:href="#fa-github"></use>
-                    </svg>
-                    <em>Atriumn</em>
-                    </a>
-                </li>
-                </ul>
-            </nav>
+                    </p>
+                    <ul>
+                        <li class="github">
+                            <a
+                            rel="external"
+                            href="https://github.com/jeff-atriumn/atriumn-p-diagrams"
+                            title="Github"
+                            >
+                            <svg class="fa-github">
+                                <use xlink:href="#fa-github"></use>
+                            </svg>
+                            <em>Atriumn</em>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
             </header>
-
             <main role="main">
             <article id="webslides" class="vertical">
-                <section>
-                <div class="wrap">
-                    <div class="grid vertical-align">
-                    <div class="column">
-                        <h4>
-                        <svg class="fa-envelope">
-                            <use xlink:href="#fa-envelope"></use>
-                        </svg>
-                        <strong>Inputs</strong>
-                        </h4>
-                        <h5 id="title"></h5>
-                        <ul class="description" id="inputs">
-                        </ul>
+                <section class="bg-apple aligncenter">
+                    <div
+                        id="title"
+                        style="
+                            position: fixed;
+                            right: 20px;
+                            bottom: 20px;
+                            color: #aaa;
+                            font-size: 5vw;
+                        "
+                        >
+                        {data.title}
                     </div>
-                    <div class="column">
-                        <figure>
-                        <img
-                            class="aligncenter"
-                            src="static/images/setup.png"
-                            alt="System Function"
-                        />
-                        </figure>
-                    </div>
-                    <div class="column">
-                        <h4>
-                        <svg class="fa-cubes">
-                            <use xlink:href="#fa-cubes"></use>
-                        </svg>
-                        <strong>Outcomes</strong>
-                        </h4>
-                        <p>
-                        The best way to <strong>inspire with your content</strong> is
-                        to connect on a personal level:
-                        </p>
-                        <ul class="description">
-                        <li>
-                            Background images:
-                            <a href="http://unsplash.com">Unsplash</a>.
-                        </li>
-                        </ul>
-                    </div>
-                    </div>
-                </div>
-                </section>
-            </article>
-            </main>
+                    <div class="wrap">
+                        <div
+                            class="grid"
+                            style="display: flex; flex-direction: column; align-items: center"
+                        >
+                            <div class="row">
+                                <h4>
+                                    <svg class="fa-music">
+                                    <use xlink:href="#fa-music"></use>
+                                    </svg>
+                                    <strong>Noise</strong>
+                                </h4>
+                                <h5 id="title"></h5>
+                                <ul class="description noise" id="noise"></ul>
+                            </div>
 
-                <script src="static/js/webslides.js"></script>
-                <script>
-                window.ws = new WebSlides();
-                </script>
-            
-                <script defer src="static/js/svg-icons.js"></script>
-                    <script src="data.js"></script>
-                <script>
-                document.getElementById('title').textContent = data.title;
+                            <div
+                            class="row"
+                            style="
+                                display: flex;
+                                justify-content: space-between;
+                                width: 80%;
+                            "
+                            >
+                                <div class="column" style="text-align: right">
+                                    <h4>
+                                    <svg class="fa-envelope">
+                                        <use xlink:href="#fa-envelope"></use>
+                                    </svg>
+                                    <strong>Inputs</strong>
+                                    </h4>
+                                    <ul class="description" id="inputs"></ul>
+                                </div>
+
+                                <div class="column">
+                                    <figure>
+                                    <div
+                                        style="
+                                        background-color: black;
+                                        color: white;
+                                        display: flex;
+                                        justify-content: center;
+                                        align-items: center;
+                                        height: 200px;
+                                        width: 200px;
+                                        "
+                                    >
+                                        {data.title}
+                                    </div>
+                                    </figure>
+                                </div>
+
+                                <div class="column" style="text-align: left">
+                                    <h4>
+                                    <svg class="fa-cubes">
+                                        <use xlink:href="#fa-cubes"></use>
+                                    </svg>
+                                    <strong>Outcomes</strong>
+                                    </h4>
+                                    <h5 id="title"></h5>
+                                    <ul class="description" id="outcomes"></ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+              </article>
+            </main>
+            <script src="static/js/webslides.js"></script>
+            <script>
+            window.ws = new WebSlides();
+            </script>
+
+            <script defer src="static/js/svg-icons.js"></script>
+            <script src="data.js"></script>
+
+            <script>
+                document.title = data.title;
                 const inputsList = document.getElementById('inputs');
                 data.inputs.forEach(input => {
                     const li = document.createElement('li');
                     li.textContent = input;
                     inputsList.appendChild(li);
                 });
-                </script>
-            </body>
-        </html>
+
+                const outcomesList = document.getElementById('outcomes');
+                data.outcomes.forEach(outcome => {
+                    const li = document.createElement('li');
+                    li.textContent = outcome;
+                    outcomesList.appendChild(li);
+                });
+
+                const noiseList = document.getElementById('noise');
+                data.noise.forEach(n => {
+                    const li = document.createElement('li');
+                    li.textContent = n;
+                    noiseList.appendChild(li);
+                });
+
+                const figureDiv = document.querySelector('.column figure div');
+                figureDiv.textContent = data.title;
+
+                const titleElement = document.getElementById('title');
+                titleElement.textContent = data.title;
+            </script>
+        </body>
+    </html>
     `;
     await fs.writeFile("./index.html", html, "utf8");
   } catch (err) {
